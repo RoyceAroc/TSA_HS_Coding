253bcd81fe6e22a1608dd942d4c29257ffa82849
Royce Arockiasamy <royceaden@gmail.com>
2021-02-13 12:57:58 -0500

    Update code.cpp

--- Task4/code.cpp
+++ Task4/code.cpp
@@ -2,15 +2,60 @@
 
 using namespace std;
 
+string positions[6] = {"President", "Vice President", "Secretary", "Treasurer", "Reporter", "Sergeant-at-Arms"};
+
+bool cmp(pair<string, int>& a, 
+         pair<string, int>& b) 
+{ 
+    return a.second > b.second; 
+} 
+
+void sort(map<string, int>& M, int current) 
+{ 
+    vector<pair<string, int>> A; 
+    for (auto& it : M) { 
+        A.push_back(it); 
+    } 
+    sort(A.begin(), A.end(), cmp); 
+    vector<pair<string, int>>::iterator first;
+    first = A.begin();
+    vector<pair<string, int>>::iterator second;
+    second = A.begin(); second++;
+    if(first->second > second->second || A.size() == 1) {
+        cout << positions[current] << ": " << first->first << ", " << first->second << endl;
+    } else {
+        cout << positions[current] << ": -tie-";
+        for (auto& it : A) { 
+            if(it.second == first->second) {
+                cout << " " + it.first;
+            }
+        }
+        cout << ", " << first->second << endl;
+    }
+    A.clear();
+} 
+
 int main()
 {
     int bal; cin >> bal;
-    string pos[6][bal*6];
+    string pos[6][bal];
     for(int i=0; i<bal; ++i) {
         for(int j=0; j<6; ++j) {
             string name; cin >> name;
-            pos[i][j] = name;
+            pos[j][i] = name;
         }
     }
-    //Alphabetical sort & Analysis TBD
+    map<string, int> occurence;
+    for(int i=0; i<6; ++i) { //set max 6 soon and 0 start
+        sort(pos[i], pos[i]+bal);
+        for(int j=0; j<bal; ++j) {
+            if(occurence.find(pos[i][j]) == occurence.end()) {
+                occurence[pos[i][j]] = 1;
+            } else {
+                occurence[pos[i][j]] += 1;
+            }
+        }
+        sort(occurence, i);
+        occurence.clear();
+    }
 }
